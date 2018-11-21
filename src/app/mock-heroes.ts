0d9671653c4a917a7e26e27dc4d67f78be12783b
Igor Garcia <36867094+igorgarciaco@users.noreply.github.com>
2018-11-21 00:50:02 -0200

    First Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Abbadon' },
+  { id: 12, name: 'Anti-Mage ' },
+  { id: 13, name: 'Axe' },
+  { id: 14, name: 'Bane' },
+  { id: 15, name: 'Blodseeker' },
+  { id: 16, name: 'Crystal Maiden' },
+  { id: 17, name: 'Drow Ranger' },
+  { id: 18, name: 'Earthshaker' },
+  { id: 19, name: 'Juggernaut' },
+  { id: 20, name: 'Spectre' }
+];
